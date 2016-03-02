e329ff1574beae239510a22f3b091a1a2445d243
Your Name <you@example.com>
2016-03-02 10:45:17 -0500

    setting up repo

---
+++ GitBash practice2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBash_practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
